3d47e37f3d31fcebf37eafec2dd4367302b85916
nomand <alexey.botkov@gmail.com>
2018-04-06 15:06:05 +1200

    Analytics calls for game start and end

---
+++ Assets/Analytics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class Analytics : MonoBehaviour {
+
+	public void LogGameStart()
+    {
+        AnalyticsEvent.GameStart();
+    }
+
+    public void LogGameComplete()
+    {
+        AnalyticsEvent.GameOver();
+    }
+}
